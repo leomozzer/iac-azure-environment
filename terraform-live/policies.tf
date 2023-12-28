f1f7bc68c2991278bcbbd712109f9fe4d085d329
leomozzer <mozzer.ferraz@gmail.com>
2023-12-28 12:05:50 -0300

    Added storage account initiative

--- terraform-live/policies.tf
+++ terraform-live/policies.tf
@@ -110,14 +110,44 @@ module "storage_account_avaliability_alert" {
   management_group_id = data.azurerm_management_group.management_group.id
 }
 
-module "storage_account_avaliability_alert_assignment" {
-  source            = "gettek/policy-as-code/azurerm//modules/def_assignment"
-  version           = "2.8.0"
-  definition        = module.storage_account_avaliability_alert.definition
+module "storage_account_monitoring_initiative" {
+  source                  = "gettek/policy-as-code/azurerm//modules/initiative"
+  initiative_name         = "storage_account_monitoring_initiative"
+  initiative_display_name = "[Monitoring]: Storage Accounts"
+  initiative_description  = "Collection of policies that deploy alerts over storage accounts"
+  initiative_category     = "Monitoring"
+  management_group_id     = data.azurerm_management_group.management_group.id
+
+  member_definitions = [
+    module.storage_account_avaliability_alert.definition,
+  ]
+}
+
+module "storage_account_monitoring_assignment" {
+  source            = "gettek/policy-as-code/azurerm//modules/set_assignment"
+  initiative        = module.storage_account_monitoring_initiative.initiative
   assignment_scope  = data.azurerm_management_group.management_group.id
   assignment_effect = "DeployIfNotExists"
+
+  # optional resource remediation inputs
+  re_evaluate_compliance = false
+  skip_remediation       = false
+  skip_role_assignment   = false
+  remediation_scope      = data.azurerm_management_group.management_group.id
 
   assignment_parameters = {
     actionGroupID = azurerm_monitor_action_group.azurerm_monitor_action_group.id
   }
 }
+
+# module "storage_account_avaliability_alert_assignment" {
+#   source            = "gettek/policy-as-code/azurerm//modules/def_assignment"
+#   version           = "2.8.0"
+#   definition        = module.storage_account_avaliability_alert.definition
+#   assignment_scope  = data.azurerm_management_group.management_group.id
+#   assignment_effect = "DeployIfNotExists"
+
+#   assignment_parameters = {
+#     actionGroupID = azurerm_monitor_action_group.azurerm_monitor_action_group.id
+#   }
+# }
