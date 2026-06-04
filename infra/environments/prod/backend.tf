a1fbb3a29544c3f251d691fe65672894118b959d
leomozzer <mozzer.ferraz@gmail.com>
2026-06-04 19:51:58 -0300

    fix: restore tfplan gitignore patterns, fix path separator, add backend init docs

--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -1,3 +1,6 @@
+# Initialize with: terraform init \
+#   -backend-config="resource_group_name=<rg>" \
+#   -backend-config="storage_account_name=<sa>"
 terraform {
   backend "azurerm" {
     resource_group_name  = ""
