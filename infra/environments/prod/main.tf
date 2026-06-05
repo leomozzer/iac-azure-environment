27934bd627c38249967762f5567af051d52ae869
leomozzer <mozzer.ferraz@gmail.com>
2026-06-05 11:49:26 -0300

    Added resource group creation

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -5,10 +5,15 @@ module "operationalinsights" {
   instance = var.instance
 }
 
+resource "azurerm_resource_group" "operationalinsights" {
+  name     = module.operationalinsights.resource_group
+  location = var.region
+}
+
 module "avm-res-operationalinsights-workspace" {
   source              = "Azure/avm-res-operationalinsights-workspace/azurerm"
   version             = "0.5.1"
-  resource_group_name = module.operationalinsights.resource_group
+  resource_group_name = azurerm_resource_group.operationalinsights.name
   location            = var.region
   name                = module.operationalinsights.log_analytics_workspace
 }
