c0d02a3e7f37cbf21642e4414fc5d9fafb518d93
leomozzer <mozzer.ferraz@gmail.com>
2026-06-20 12:08:18 -0300

    Adding amba deployment

--- infra/modules/amba/outputs.tf
+++ infra/modules/amba/outputs.tf
@@ -2,8 +2,3 @@ output "resource_group_name" {
   description = "The name of the resource group containing the AMBA resources."
   value       = module.naming.resource_group
 }
-
-output "resource_group_id" {
-  description = "The resource ID of the resource group containing the AMBA resources."
-  value       = module.avm-res-resources-resourcegroup.resource.id
-}
