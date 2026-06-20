6d941285d4b26145a96d3fc50b7b97f5b5914246
leomozzer <mozzer.ferraz@gmail.com>
2026-06-20 12:04:37 -0300

    Adding amba deployment

--- infra/modules/amba/main.tf
+++ infra/modules/amba/main.tf
@@ -5,18 +5,11 @@ module "naming" {
   instance = var.instance
 }
 
-module "avm-res-resources-resourcegroup" {
-  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
-  version = "0.4.0"
-
-  name     = module.naming.resource_group
-  location = var.region
-}
-
 module "avm_ptn_monitoring_amba_alz" {
   source  = "Azure/avm-ptn-monitoring-amba-alz/azurerm"
   version = "0.3.0"
 
   location                   = var.region
   root_management_group_name = var.root_management_group_name
+  resource_group_name        = module.naming.resource_group
 }
